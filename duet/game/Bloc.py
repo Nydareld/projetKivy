ef73d30971f117fd4ed2d53859b8d83b44651db3
Theo Guerin <guerin@info27-11.iut45.univ-orleans.fr>
2017-01-24 15:27:51 +0100

    Modification du type de game de widget en screen

--- duet/game/Bloc.py
+++ duet/game/Bloc.py
@@ -3,9 +3,9 @@ from kivy.properties import NumericProperty, ObjectProperty
 from kivy.animation import Animation
 
 class Bloc(Widget):
-    width : NumericProperty(0)
-    height : NumericProperty(0)
-    position : NumericProperty(0)
-    positionLaterale : NumericProperty(0)
-    bouncy : NumericProperty(0)
-    rotation : NumericProperty(0)
+    width = NumericProperty(0)
+    height = NumericProperty(0)
+    position = NumericProperty(0)
+    positionLaterale = NumericProperty(0)
+    #bouncy = NumericProperty(0)
+    #rotation = NumericProperty(0)
