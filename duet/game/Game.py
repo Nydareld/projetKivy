8be8c7394d40fdb248083a4bf81baa7f0edd8af7
Théo Guerin <theo.guerin@nr-communication.fr>
2017-02-08 17:02:23 +0100

    initialisation du niveau par level

--- duet/game/Game.py
+++ duet/game/Game.py
@@ -8,18 +8,33 @@ from random import randint
 
 class Game(Screen):
     # Offset entre chaque bloc
-    offset = NumericProperty(dp(100))
+    offset = NumericProperty(dp(200))
     # marge avant le premier bloc
-    startx = NumericProperty(dp(200))
+    startx = NumericProperty(dp(1000))
+    blocs = []
 
     def __init__(self, **kwargs):
         Builder.load_file("duet/game/game.kv")
 
         super(Game, self).__init__(**kwargs)
-        self.reset()
+        print("init")
+        # self.reset()
 
-    def reset(self):
-        for i in range(10):
-            rand = randint(0, 10)
-            print(rand)
-            Bloc(i,50*rand,(200,30),self)
+    def reset(self,levelData=None):
+
+        self.blocs = []
+
+        if(levelData == None):
+            for i in range(10):
+                # 0--50<--->250--300<--->500--550<--->750--800
+                gauche = [50,300,550]
+                rand = randint(0, 2)
+                bloc = Bloc(i,gauche[rand],(200,30),self)
+                self.blocs.append(bloc)
+        else :
+            for blocData in levelData:
+                bloc = Bloc(blocData[0],blocData[1],blocData[2],self)
+                self.blocs.append(bloc)
+
+        for bloc in self.blocs:
+            bloc.move()
